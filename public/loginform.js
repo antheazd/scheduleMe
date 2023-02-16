09340e09195f8758da16951593bf90476ec99598
Anthea <@anthea.zubcic@gmail.com>
2023-02-16 20:00:40 +0100

    Edited login.js

--- public/loginform.js
+++ public/loginform.js
@@ -14,9 +14,37 @@
 
 
 class LoginForm extends React.Component {
+
   render() {
-    
     return (
+      <div className="half_page">
+        <div className="ui attached message">
+          <div className="content">
+            <div className="header">Welcome to our site!</div>
+                <p>Log into your scheduleMe account</p></div></div>
+                <form className="ui form attached fluid segment" method="post">
+                    <div className="field">
+                        <label>Email</label>
+                        <div className="ui input">
+                            <input type="email" id="userEmail" name = "email" placeholder="Email address"/>
+                        </div>
+                    </div>
+                    <div className="field">
+                        <label>Password</label>
+                        <div className="ui input">
+                            <input type="password" id="userPassword" name = "password" placeholder="Password"/>
+                        </div>
+                    </div>
+                    <button className="ui blue button">Log in</button>
+                </form>
+                <div className="ui warning bottom attached message">
+                    <i aria-hidden="true" className="help icon"></i>Don't have an account? <a href="/login">Sign up</a> here instead.
+                </div>
+      </div>
+  
+    );}
+    
+    /*return (
       <div className="full_page">
       <div className="half_page">
       <div className="ui center aligned container"> 
@@ -33,7 +61,7 @@ class LoginForm extends React.Component {
           <Button />
       </form>
   </div></div></div>
-    );}
+    );} */
 }
 
 let container = document.getElementById('loginform');
